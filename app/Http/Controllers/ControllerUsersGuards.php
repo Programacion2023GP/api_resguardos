e7a4f0b69894e36b57091ac3dfadcd0c5b989179
Luis Angel <[200~>
2024-01-17 08:29:17 -0600

    tipo finalizado

--- app/Http/Controllers/ControllerUsersGuards.php
+++ app/Http/Controllers/ControllerUsersGuards.php
@@ -40,9 +40,11 @@ class ControllerUsersGuards extends Controller
     {
         $response->data = ObjResponse::DefaultResponse();
         try {
-            $list = User::select('users.*', 'user_guards.*', 'guards.*','user_guards.id as idguard', 'user_guards.active as used')
+            $list = User::select('users.*', 'user_guards.*', 'guards.*','user_guards.id as idguard', 'user_guards.active as used', 'types.name as Tipo')
             ->join('user_guards', 'user_guards.user_id', '=', 'users.id')
             ->join('guards', 'user_guards.guards_id', '=', 'guards.id')
+            ->leftjoin('types', 'types.id', '=', 'guards.type_id')
+
             ->where('users.id', $id) // Filtrar por el ID proporcionado
             ->orderBy('users.id', 'desc') // Ordenar por el campo ID de users (o el campo deseado)
             ->orderByRaw('user_guards.active DESC') // Ordena por 'active' de forma descendente (1 antes que 0)
