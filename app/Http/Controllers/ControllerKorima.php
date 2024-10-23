21ea7ce514b40a5c25566b11e534f12f0cdcee4e
Luis Angel <programacion@gomezpalacio.gob.mx>
2024-10-23 15:36:41 -0600

    cambiooo

--- app/Http/Controllers/ControllerKorima.php
+++ app/Http/Controllers/ControllerKorima.php
@@ -163,7 +163,7 @@ class ControllerKorima extends Controller
              
                 $guard->korima = $request->korima;
                 $guard->observation = $request->observation;
-                if ($request->hasFile("picture") || $request->korima!= null) {
+                if ($request->hasFile("picture") || $request->observation!= "null") {
                     return $request->all();
                     // Si al menos uno de los campos tiene un valor, actualiza el registro
                     // $guard->update();                
