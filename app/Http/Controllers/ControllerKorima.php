09d7cbaeb96a32d79e39306713b82a79c641ee17
Luis Angel <programacion@gomezpalacio.gob.mx>
2025-02-24 10:34:50 -0600

    cambios

--- app/Http/Controllers/ControllerKorima.php
+++ app/Http/Controllers/ControllerKorima.php
@@ -30,15 +30,15 @@ class ControllerKorima extends Controller
 
                 // Genera la URL correcta con `storage/`
                 $guard->picture = url("storage/Korima/{$request->payroll}/{$request->korima}/{$nuevoNombreArchivo}");
-                
+
                 // $archivo->move(public_path("Korima/{$request->payroll}/{$request->korima}"), $nuevoNombreArchivo);
 
                 // // $guard->picture = asset("Korima/{$request->payroll}/{$request->korima}/{$nuevoNombreArchivo}");
                 // $guard->picture = url("storage/Korima/{$request->payroll}/{$request->korima}/{$nuevoNombreArchivo}");
                 // Mueve el archivo al directorio público
                 // Mueve el archivo a la ruta deseada
-//                 $archivo->move(public_path("Korima/{$request->payroll}/{$request->korima}"), $nuevoNombreArchivo);
-//  // $guard->picture = "http://localhost:8000/Korima/{$request->payroll}/{$request->korima}/{$nuevoNombreArchivo}";
+                //                 $archivo->move(public_path("Korima/{$request->payroll}/{$request->korima}"), $nuevoNombreArchivo);
+                //  // $guard->picture = "http://localhost:8000/Korima/{$request->payroll}/{$request->korima}/{$nuevoNombreArchivo}";
                 // Guarda el valor de 'korima' en el objeto $guard
 
                 // Construye la URL completa de la imagen
@@ -71,7 +71,7 @@ class ControllerKorima extends Controller
 
                 // Genera la URL correcta con `storage/`
                 $guard->tag_picture = url("storage/Korima/{$request->payroll}/{$request->korima}/{$nuevoNombreArchivo}");
-                
+
                 // Guarda la URL completa de la imagen en el objeto $guard
                 // $guard->tag_picture = asset("Korima/{$request->payroll}/{$request->korima}/{$nuevoNombreArchivo}");
                 // $archivo->move(public_path("Korima/{$request->payroll}/{$request->korima}"), $nuevoNombreArchivo);
@@ -150,16 +150,16 @@ class ControllerKorima extends Controller
                     // Obtén el nombre original del archivo y genera un nuevo nombre
                     $nombreArchivo = $archivo->getClientOriginalName();
                     $nuevoNombreArchivo = date('Y-m-d_H-i-s') . '_' . $nombreArchivo;
-                    
+
                     // Mueve el archivo a la ruta deseada dentro de la carpeta `public`
                     // $archivo->move(public_path("Korima/{$request->payroll}/{$request->korima}"), $nuevoNombreArchivo);
-                    
+
                     // // Construye la URL pública correcta
                     // $guard->picture = url("storage/Korima/{$request->payroll}/{$request->korima}/{$nuevoNombreArchivo}");
                     $archivo->storeAs("public/Korima/{$request->payroll}/{$request->korima}", $nuevoNombreArchivo);
 
-// Genera la URL correcta con `storage/`
-$guard->picture = url("storage/Korima/{$request->payroll}/{$request->korima}/{$nuevoNombreArchivo}");
+                    // Genera la URL correcta con `storage/`
+                    $guard->picture = url("storage/Korima/{$request->payroll}/{$request->korima}/{$nuevoNombreArchivo}");
 
 
                     // Guarda el valor de 'korima' en el objeto $guard
@@ -184,34 +184,34 @@ $guard->picture = url("storage/Korima/{$request->payroll}/{$request->korima}/{$n
 
                     // Genera la URL correcta con `storage/`
                     $guard->tag_picture = url("storage/Korima/{$request->payroll}/{$request->korima}/{$nuevoNombreArchivo}");
-                    
+
                     // Guarda el valor de 'korima' en el objeto $guard
 
                     // Construye la URL completa de la imagen
                     // $guard->tag_picture = "http://localhost:8000/Korima/{$request->payroll}/{$request->korima}/{$nuevoNombreArchivo}";
 
                     // O si estás usando el dominio de producción
-                //     $guard->tag_picture = "https://api.resguardosinternos.gomezpalacio.gob.mx/public/Korima/{$request->payroll}/{$request->korima}/{$nuevoNombreArchivo}";
-                // }
-                // Verifica y actualiza los campos 'korima' y 'observation' si están presentes en la solicitud
+                    //     $guard->tag_picture = "https://api.resguardosinternos.gomezpalacio.gob.mx/public/Korima/{$request->payroll}/{$request->korima}/{$nuevoNombreArchivo}";
+                    // }
+                    // Verifica y actualiza los campos 'korima' y 'observation' si están presentes en la solicitud
 
-                $guard->korima = $request->korima;
-                $guard->observation = $request->observation ?? $guard->observation;
-                if ($request->hasFile("picture") || $request->hasFile("tag_picture") || $request->observation) {
-                    $response = ObjResponse::CorrectResponse();
-                    $response["message"] = 'Datos actualizados exitosamente';
-                    $response["alert_text"] = "El registro se ha actualizado correctamente";
-                    // Si al menos uno de los campos tiene un valor, actualiza el registro
-                    $guard->update();
-                } else {
-                    // Si no se proporciona el archivo
-                    $response = ObjResponse::CatchResponse("No se ha proporcionado ninguna imagen.");
+                    $guard->korima = $request->korima;
+                    $guard->observation = $request->observation ?? $guard->observation;
+                    if ($request->hasFile("picture") || $request->hasFile("tag_picture") || $request->observation) {
+                        $response = ObjResponse::CorrectResponse();
+                        $response["message"] = 'Datos actualizados exitosamente';
+                        $response["alert_text"] = "El registro se ha actualizado correctamente";
+                        // Si al menos uno de los campos tiene un valor, actualiza el registro
+                        $guard->update();
+                    } else {
+                        // Si no se proporciona el archivo
+                        $response = ObjResponse::CatchResponse("No se ha proporcionado ninguna imagen.");
+                    }
+                    // Guarda los cambios en la base de datos
+
+                    // Respuesta correcta
                 }
-                // Guarda los cambios en la base de datos
-
-                // Respuesta correcta
             }
-        }
         } catch (\Exception $ex) {
             // Manejo de errores y excepciones
             $response = ObjResponse::CatchResponse($ex->getMessage());
@@ -230,7 +230,7 @@ $guard->picture = url("storage/Korima/{$request->payroll}/{$request->korima}/{$n
                 $korima->update();
             } else {
                 $korima = new Korima();
-                $korima->korima =$request->korima;
+                $korima->korima = $request->korima;
                 $korima->motive_down = $request->motive_down;
                 $korima->save();
             }
@@ -246,24 +246,23 @@ $guard->picture = url("storage/Korima/{$request->payroll}/{$request->korima}/{$n
     public function transfer(Request $request, Response $response)
     {
         $response->data = ObjResponse::DefaultResponse();
-        $user=User::where('id', $request->value)->first();
+        $user = User::where('id', $request->value)->first();
         try {
             $korima = Korima::find($request->id);
             if ($korima) {
-             $korima->trauser_id = $user->name;
-             $korima->motive_down ='transferencia de resguardo a ' . $user->name;
-             $korima->user_id = $request->name;
-             
-             $korima->motivetransfer = $request->motivetransfer;
-
-             
-            $korima->update();
-            }
-            else{
-                $korima = new Korima();
-                $korima->korima =$request->NumeroEconomicoKorima;
                 $korima->trauser_id = $user->name;
-                $korima->motive_down ='transferencia de resguardo a ' . $user->name;
+                $korima->motive_down = 'transferencia de resguardo a ' . $user->name;
+                $korima->user_id = $request->name;
+
+                $korima->motivetransfer = $request->motivetransfer;
+
+
+                $korima->update();
+            } else {
+                $korima = new Korima();
+                $korima->korima = $request->NumeroEconomicoKorima;
+                $korima->trauser_id = $user->name;
+                $korima->motive_down = 'transferencia de resguardo a ' . $user->name;
                 $korima->user_id = $request->name;
                 $korima->motivetransfer = $request->motivetransfer;
 
@@ -283,23 +282,21 @@ $guard->picture = url("storage/Korima/{$request->payroll}/{$request->korima}/{$n
         $response->data = ObjResponse::DefaultResponse();
         try {
             $korima = Korima::find($request->id);
-            $admin = Auth::user()->role ==1 || Auth::user()->role ==2 ?true :false;
+            $admin = Auth::user()->role == 1 || Auth::user()->role == 2 ? true : false;
             $korima->archivist = $admin ? 1 : null;
             if ($korima) {
                 if (!$admin) {
-                $korima->autorized = $request->option==1?true:null;
+                    $korima->autorized = $request->option == 1 ? true : null;
                 }
-                if ($request->option==0) {
+                if ($request->option == 0) {
                     $korima->archivist = $admin ? 0 : null;
                     if (!$admin) {
                         # code...
                         $korima->motive_down = null;
                         $korima->trauser_id = null;
                         $korima->motivetransfer = null;
-                        
                     }
                     $korima->motivearchivist = $admin ? $request->motivearchivist : null;
-                    
                 }
                 $korima->update();
             }
